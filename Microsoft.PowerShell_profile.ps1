d70960a1578147ad4d2fc0d852b6f0cb88f8bcc8
Abdul Hakim <alarwasyi98@gmail.com>
2024-09-29 11:37:28 +0700

    perf: resolve pipeline issue

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,25 @@
 
 ### ENVIRONMENT VARIABLES ###
 
+Set-Item -Force -Path "env:CONFIG_HOME" -Value "$HOME/.config"
+
+# Utility Functions
+function Test-CommandExists {
+    param($command)
+    $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
+    return $exists
+}
+
+# Editor Configuration
+$EDITOR = if (Test-CommandExists nvim) { 'nvim' }
+elseif (Test-CommandExists pvim) { 'pvim' }
+elseif (Test-CommandExists vim) { 'vim' }
+elseif (Test-CommandExists vi) { 'vi' }
+elseif (Test-CommandExists code) { 'code' }
+elseif (Test-CommandExists notepad++) { 'notepad++' }
+elseif (Test-CommandExists sublime_text) { 'sublime_text' }
+else { 'notepad' }
+
 # Exporting chocolatey profile to enable tab-completion
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -14,61 +33,70 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 ### ALIASES ###
-Set-Alias tt tree
-Set-Alias ll ls
-Set-Alias -Name la -Value "Get-ChildItem -Path . -Force | Format-Table -AutoSize"
-Set-Alias -Name ln -Value "Get-ChildItem -Name | Format-Table -Autosize"
+Set-Alias -Name tt -Value tree
+Set-Alias -Name ll -Value ls
 
-Set-Alias vim nvim
-Set-Alias vi nvim
-Set-Alias cat bat
-Set-Alias h -Value Get-History
+Set-Alias -Name vim -Value $EDITOR
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name cat -Value bat
+Set-Alias -Name h -Value Get-History
 
-Set-Alias -Name touch -Value "New-Item -Type File"
-Set-Alias -Name docs -Value "Set-Location -Path $HOME\Documents"
-Set-Alias -Name pict -Value "Set-Location -Path $HOME\Pictures"
-Set-Alias -Name ep -Value "nvim $PROFILE"
-Set-Alias -Name sedit -Value "vim $HOME\.config\starship.toml"
+### HANDY ALIASES ###
+#Set-Alias -Name touch -Value "New-Item -Type File"
+Set-Alias -Name ep -Value Edit-Profile
+Set-Alias -Name sedit -Value Edit-Starship
 
 # Set UNIX-like aliases for the admin command, See function below
 # so sudo <command> will run the command with elevated rights.
 Set-Alias -Name su -Value admin
 
-### UNUSED ALIASES ###
-#Set-Alias -Name wa -Value Start-WhatsApp
-
 ### FUNCTIONS ###
 
+# pwsh
+Function Edit-Profile { vim $PROFILE }
+
+# starship
+Function Edit-Starship { vim $HOME\.config\starship.toml }
+
 # Git
-function gs { git status }
-function ga { git add . }
-function gcm { param($m) git commit -m "$m" }
-function gp { git push }
-function g { z Github }
-function gcl { git clone "$args" }
-function gcom {
+Function gs { git status }
+Function ga { git add . }
+Function gcm { param($m) git commit -m "$m" }
+Function gp { git push }
+Function g { z Github }
+Function gcl { git clone "$args" }
+Function gcom {
     git add .
     git commit -m "$args"
 }
-function lazyg {
+Function lazyg {
     git add .
     git commit -m "$args"
     git push
 }
 
 # fzf
-function bfzf {
+Function bfzf {
     fzf --preview="bat --decorations=always --color=always {}"
 }
 
-function fzfvim {
+Function fzfvim {
     nvim (fzf --preview="bat --decorations=always --color=always {}")
+}
+
+
+# Easy Navigate
+Function docs {
+  Set-Location $HOME/Documents
+}
+Function power {
+  Set-Location $HOME/Documents/PowerShell
 }
 
 ### SPECIAL FUNCTIONS ###
 
 # Run as Administrator
-function admin {
+Function admin {
     if ($args.Count -gt 0) {
         $argList = "& '$args'"
         Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
@@ -78,8 +106,15 @@ function admin {
     }
 }
 
+# Reload PowerShell Profiles for All Users
+Function Update-Profile {
+    # Memuat ulang profil PowerShell
+    . $PROFILE
+    Write-Output "Profile Reloaded"
+}
+
 # Print command location
-function which {
+Function which {
     param (
         [Parameter(Mandatory = $true, Position = 0)]
         [string]$command  
@@ -94,44 +129,20 @@ function which {
         Write-Host "${command} not found"
     }
 }
-
-# Reload PowerShell Profiles for All Users
-function Update-Profile {
-    # Memuat ulang profil PowerShell
-    . $PROFILE
-    Write-Output "Profile Reloaded"
-}
-
+function touch($file) { "" | Out-File $file -Encoding ASCII }
 # Locate file quickly
-function ff($name) {
+Function ff ($name) {
     Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
         Write-Output "$($_.FullName)"
     }
 }
 
 # Extract ZIP File
-function unzip ($file) {
+Function unzip ($file) {
     Write-Output("Extracting", $file, "to", $pwd)
     $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
     Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
-
-# Start WhatsApp Desktop
-# NOTE: Disabled due to performance issues
-## Start-Process Appx
-#function Start-WhatsApp {
-#    # Get package's name
-#    $whatsappPackage = Get-AppxPackage -Name "*WhatsAppDesktop*"
-#    # Check if package found
-#    if ($whatsappPackage) {
-#        # Run WhatsApp
-#        $packageFullName = $whatsappPackage.PackageFamilyName
-#        Start-Process "shell:appsFolder\$packageFullName!App" -ErrorAction SilentlyContinue
-#        Write-Output "WhatsApp Started"
-#    } else {
-#        Write-Output "WhatsApp not found!"
-#    }
-#}
 
 ### MODULES IMPORTER ###
 
@@ -139,17 +150,17 @@ function unzip ($file) {
 Import-Module Terminal-Icons
 
 # PSReadline; fish-like capability
-Import-Module PSReadline
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -PredictionViewStyle InlineView 
 Set-PsReadlineOption -PredictionSource History
-# Set-PSReadlineOption -PredictionViewStyle InlineView
 
 # Command Not Found (PowerToys)
 Import-Module -Name Microsoft.WinGet.CommandNotFound
-# f45873b3-b655-43a6-b217-97c00aa0db58
 
 ### INVOCATIONS ###
+
+# colorscripts
+Show-ColorScript -Name alpha
 
 # starship
 Invoke-Expression (&starship init powershell)
